dba018bcf9c9c6c5e1ef9a13c62ba5e806027384
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2022-02-12 13:51:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,9 @@ paddle_height = 100
 p1_x_pos = WIDTH - paddle_width - 10
 p1_y_pos = HEIGHT / 2 - paddle_height / 2
 
+p2_x_pos = WIDTH - paddle_width - 10
+p2_y_pos = WIDTH / 2 - paddle_height / 2
+
 p1_score = 0
 p2_score = 0
 
@@ -41,3 +44,7 @@ ball_x_vel = -10
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 #drawing objects
+def draw_objects():
+    pygame.draw.rect(screen, WHITE, (int(p1_x_pos), int(p1_y_pos), paddle_width, paddle_height))
+    pygame.draw.rect(screen, WHITE, (int(p2_x_pos), int(p2_y_pos), paddle_width, paddle_height))
+    pygame.draw.circle(screen, WHITE)
